6c5323548791c3765c90dc09e3e1a6d0bab683a7
Matti Nilsson <matti.nilsson980105@gmail.com>
2020-11-01 18:42:52 +0100

    broke out dice to its own class

---
+++ scripts/Dice.js
@@ -0,0 +1,69 @@
+import { prefabDice } from "./util.js";
+
+export default class Dice{
+  constructor(){
+    this.diceCollection = prefabDice();
+    this.createDice();
+    this.start();
+  }
+
+  start(){
+    $(".roll").click(() => {this.rollDice()});
+  }
+
+  createDice(){
+    $(".dice").empty();
+    for(let i = 0; i < 5; i++){
+      let num = this.diceCollection[i].nr;
+      let img = this.diceCollection[i].locked ? `images/inverted-dice-${num}.svg` : `images/dice-six-faces-${num}.svg`
+      $(".dice").append(`
+        <img class="dice-${i} die" src=${img}>
+      `)
+      $(`.dice-${i}`).click(() => this.lockDice(i))
+    }
+  }
+
+  lockDice(i){
+    this.diceCollection[i].locked = !this.diceCollection[i].locked;
+    let nr = this.diceCollection[i].nr;
+    let img = this.diceCollection[i].locked ? `images/inverted-dice-${nr}.svg` : `images/dice-six-faces-${nr}.svg`
+    $(`.dice-${i}`).attr("src", img)
+  }
+
+  rollDice(){
+    const rerole = (i, real) => {
+      if(!this.diceCollection[i].locked){
+        let num = Math.floor(Math.random() * 5);
+        this.diceCollection[i].nr = num + 1;
+        if(!real){
+          $(`.dice-${i}`).attr("src", `images/dice-six-faces-${num + 1}.svg`).css({
+            position: "relative",
+          })
+          $(`.dice-${i}`).animate({
+            top: num * 5 + 35 + "px",
+          }, 100)
+        }else{
+          $(`.dice-${i}`).attr("src", `images/dice-six-faces-${num + 1}.svg`).css({
+            position: "relative",
+          })
+          $(`.dice-${i}`).animate({
+            top: 0,
+          }, 100)
+        }
+        
+      }
+    }
+
+    for(let i = 0; i < 5; i++){ 
+      let interval = setInterval(() => {
+        rerole(i, false)
+      }, 100)
+      setTimeout(() => {
+        console.log("end")
+        clearInterval(interval);
+        rerole(i, true)
+      }, 1000 + ((i + 1) * 120))
+    }
+    console.log(this.diceCollection)
+  }
+}
